b7e5ff598dacf9fadabcd2f2d6ea9fbb78981914
Moises De Leon <mdeleon5826@gmail.com>
2022-08-17 20:20:48 -0400

    created teh initial ecommerce page with deal descriptions and a few popular items

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    flex: 1;
+    margin: 5px;
+    height: 60vh;
+    /* position: relative; */
+`
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    /* object-fit: cover; */
+`
+const Info = styled.p`
+    /* position: absolute; */
+    /* top: 0; */
+    /* left: 0; */
+    /* width: 100%; */
+    /* height: 100%; */
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+const Title = styled.h1`
+    /* color: white; */
+    /* margin-bottom: 20px; */
+`
+const Button = styled.button`
+    color: black;
+    font-weight: 600;
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+`
+
+const CategoryItem = ({item}) => {
+  return (
+    <Container>
+        <Image src={item.image}/>
+        <Info>
+            <Title>{item.title}</Title>
+        <Button>SHOP NOW</Button>
+        </Info>
+    </Container>
+  )
+}
+
+export default CategoryItem
---
+++ src/data.js
@@ -0,0 +1,82 @@
+export const sliderItems = [
+    {
+        id: 1,
+        image: "/images/jeep-1.png",
+        title: "FALL SALE",
+        desc: "DON'T COMPROMISE ON PERFORMANCE! GET FLAT 15% OFF ON FIRST PURCHASE",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        image: "/images/jeep-3.png",
+        title: "POPULAR BUYS",
+        desc: "BROWSE THROUGH THE CATALOG OF OUR MOST POPULAR ITEMS",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        image: "/images/j-front-img1.png",
+        title: "WEEKLY SALE",
+        desc: "ENJOY HALF OFF ON SELECTED ITEMS!",
+        bg: "fbf0f4",
+    },
+]
+
+
+export const categories = [
+    {
+        id: 1,
+        image: "/images/jeep-A1.png",
+        title: "ACCESSORIES",
+    },
+    {
+        id: 2,
+        image: "/images/jeep-W2.png",
+        title: "WHEELS & RIMS",
+    },
+    {
+        id: 3,
+        image: "/images/jeep-L1.png",
+        title: "LIFT KITS",
+    },
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        image: "/images/jeep-P1.jpg",
+    },
+    {
+        id: 2,
+        image: "/images/jeep-P2.jpg",
+    },
+    {
+        id: 3,
+        image: "/images/jeep-P3.jpg",
+    },
+    {
+        id: 4,
+        image: "/images/jeep-P4.jpg",
+    },
+    {
+        id: 5,
+        image: "/images/jeep-P5.jpg",
+    },
+    {
+        id: 6,
+        image: "/images/jeep-P6.jpg",
+    },
+    {
+        id: 7,
+        image: "/images/jeep-P7.jpg",
+    },
+    {
+        id: 8,
+        image: "/images/jeep-P8.jpg",
+    },
+    // {
+    //     id: 9,
+    //     image: "",
+    // },
+]
+
